8695f0cff0810d5dfa21a0cca3b02cc9f5610d2e
Trisha Gee <trisha.gee@gmail.com>
2019-09-19 17:12:29 +0200

    Adding lombok because it makes logging far tidier

--- client/src/main/java/com/mechanitis/demo/client/WebSocketStockClient.java
+++ client/src/main/java/com/mechanitis/demo/client/WebSocketStockClient.java
@@ -1,11 +1,13 @@
 package com.mechanitis.demo.client;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 
 import java.io.IOException;
 import java.time.Duration;
 
+@Log4j2
 public class WebSocketStockClient implements StockClient{
     private final WebClient webClient;
 
@@ -16,10 +18,11 @@ public class WebSocketStockClient implements StockClient{
     @Override
     public Flux<Double> pricesFor(String symbol) {
         return webClient.get()
-                .uri("http://localhost:8080/stocks/{symb}", symbol)
-                .retrieve()
-                .bodyToFlux(Double.class)
-                .retryBackoff(5, Duration.ofSeconds(1), Duration.ofSeconds(5))
-                .doOnError(IOException.class, e -> e.printStackTrace());
+                        .uri("http://localhost:8080/stocks/{symb}", symbol)
+                        .retrieve()
+                        .bodyToFlux(Double.class)
+                        .retryBackoff(5, Duration.ofSeconds(1), Duration.ofSeconds(5))
+                        .doOnError(IOException.class,
+                                   e -> log.info(() -> "Closing stream for " + symbol + ". Received " + e.getMessage()));
     }
 }
