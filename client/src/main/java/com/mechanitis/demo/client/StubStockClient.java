cb12ab70e1c45512900d867be621882fa144f7c9
Trisha Gee <trisha.gee@gmail.com>
2019-09-19 12:17:38 +0200

    Wiring up the stub data to the JavaFX chart. This does not work the way I want. Not sure why I can't have the types I want on the axes, nor does the fake data look the way I want. But it does work end to end.

--- client/src/main/java/com/mechanitis/demo/client/StubStockClient.java
+++ client/src/main/java/com/mechanitis/demo/client/StubStockClient.java
@@ -2,15 +2,16 @@ package com.mechanitis.demo.client;
 
 import reactor.core.publisher.Flux;
 
+import java.time.Duration;
 import java.util.Random;
 
 
 public class StubStockClient {
     public Flux<Double> pricesFor(String ticker) {
         final Random random = new Random();
-        return Flux.generate(() -> 0.0, (state, sink) -> {
+        return Flux.<Double, Double>generate(() -> 0.0, (state, sink) -> {
             sink.next(state);
             return state + random.nextDouble();
-        });
+        }).delayElements(Duration.ofSeconds(1L));
     }
 }
--- ui/src/main/java/com/mechanitis/demo/ui/StockController.java
+++ ui/src/main/java/com/mechanitis/demo/ui/StockController.java
@@ -1,5 +1,7 @@
 package com.mechanitis.demo.ui;
 
+import com.mechanitis.demo.client.StubStockClient;
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -7,33 +9,49 @@ import javafx.scene.chart.LineChart;
 import javafx.scene.chart.XYChart;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+import java.util.function.Consumer;
+
 import static javafx.collections.FXCollections.observableArrayList;
 
 @Component
 public class StockController {
 
     @FXML
-    private LineChart<String, Number> chart;
+    private LineChart<String, String> chart;
 
     //this is the controller, and what it does is wires stuff together
     @FXML
     public void initialize() {
         //series is the UI (View) element
-        final XYChart.Series<String, Number> series = new XYChart.Series<>();
+        final XYChart.Series<String,String> series = new XYChart.Series<>();
         //needs to be wired in to the data (Model)
-        series.setData(new StockPrice().getData());
+        StockPriceWatcher stockPriceWatcher = new StockPriceWatcher();
+        series.setData(stockPriceWatcher.getData());
 
         //a chart supports more than one series of data, so the data for the chart is a list of series
         //but we only have one element in this list, our single series
-        ObservableList<XYChart.Series<String, Number>> allSeriesForChart = observableArrayList(series);
+        ObservableList<XYChart.Series<String,String>> allSeriesForChart = observableArrayList(series);
         chart.setData(allSeriesForChart);
 
-
+        StubStockClient stubStockClient = new StubStockClient();
+        stubStockClient.pricesFor("FAKE").subscribe(stockPriceWatcher);
     }
 
-    private class StockPrice {
-        public ObservableList<XYChart.Data<String, Number>> getData() {
-            return FXCollections.observableArrayList();
+    private class StockPriceWatcher implements Consumer<Double> {
+
+        private ObservableList<XYChart.Data<String, String>> prices = FXCollections.observableArrayList();
+
+        public ObservableList<XYChart.Data<String, String>> getData() {
+            return prices;
+        }
+
+        @Override
+        public void accept(Double aDouble) {
+            System.out.println("aDouble = " + aDouble);
+            Platform.runLater(() -> prices.add(new XYChart.Data<String, String>(String.valueOf(LocalDateTime.now().getSecond()),
+                                                                                String.valueOf(aDouble))));
+//            Platform.runLater(() -> prices.add(new XYChart.Data<String, String>("0", "1")));
         }
     }
 }
