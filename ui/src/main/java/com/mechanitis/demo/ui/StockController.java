aaf6258194ed048e9f645392ca8385f1363ebb2a
Trisha Gee <trisha.gee@gmail.com>
2019-09-19 12:34:09 +0200

    Fixes the axis problem. Plus some simplification to the controller

--- ui/src/main/java/com/mechanitis/demo/ui/StockController.java
+++ ui/src/main/java/com/mechanitis/demo/ui/StockController.java
@@ -12,26 +12,28 @@ import org.springframework.stereotype.Component;
 import java.time.LocalDateTime;
 import java.util.function.Consumer;
 
+import static java.lang.String.valueOf;
+import static java.time.LocalDateTime.*;
 import static javafx.collections.FXCollections.observableArrayList;
 
 @Component
 public class StockController {
 
     @FXML
-    private LineChart<String, String> chart;
+    private LineChart<String, Double> chart;
 
     //this is the controller, and what it does is wires stuff together
     @FXML
     public void initialize() {
         //series is the UI (View) element
-        final XYChart.Series<String,String> series = new XYChart.Series<>();
+        final XYChart.Series<String, Double> series = new XYChart.Series<>();
         //needs to be wired in to the data (Model)
         StockPriceWatcher stockPriceWatcher = new StockPriceWatcher();
         series.setData(stockPriceWatcher.getData());
 
         //a chart supports more than one series of data, so the data for the chart is a list of series
         //but we only have one element in this list, our single series
-        ObservableList<XYChart.Series<String,String>> allSeriesForChart = observableArrayList(series);
+        ObservableList<XYChart.Series<String, Double>> allSeriesForChart = observableArrayList(series);
         chart.setData(allSeriesForChart);
 
         StubStockClient stubStockClient = new StubStockClient();
@@ -40,18 +42,15 @@ public class StockController {
 
     private class StockPriceWatcher implements Consumer<Double> {
 
-        private ObservableList<XYChart.Data<String, String>> prices = FXCollections.observableArrayList();
+        private ObservableList<XYChart.Data<String, Double>> prices = FXCollections.observableArrayList();
 
-        public ObservableList<XYChart.Data<String, String>> getData() {
+        ObservableList<XYChart.Data<String, Double>> getData() {
             return prices;
         }
 
         @Override
-        public void accept(Double aDouble) {
-            System.out.println("aDouble = " + aDouble);
-            Platform.runLater(() -> prices.add(new XYChart.Data<String, String>(String.valueOf(LocalDateTime.now().getSecond()),
-                                                                                String.valueOf(aDouble))));
-//            Platform.runLater(() -> prices.add(new XYChart.Data<String, String>("0", "1")));
+        public void accept(Double price) {
+            Platform.runLater(() -> prices.add(new XYChart.Data<>(valueOf(now().getSecond()), price)));
         }
     }
 }
