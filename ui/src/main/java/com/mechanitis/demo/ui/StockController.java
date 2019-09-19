14b367f06d99cb5ff122795e88be7a60d299396a
Trisha Gee <trisha.gee@gmail.com>
2019-09-19 13:11:11 +0200

    Chart with two series

--- ui/src/main/java/com/mechanitis/demo/ui/StockController.java
+++ ui/src/main/java/com/mechanitis/demo/ui/StockController.java
@@ -24,16 +24,19 @@ public class StockController {
     //this is the controller, and what it does is wires stuff together
     @FXML
     public void initialize() {
-        final String symbol = "FAKE";
+        final String symbol1 = "FAKE";
+        final String symbol2 = "DUMMY";
 
-        final StockPriceWatcher stockPriceWatcher = new StockPriceWatcher(symbol);
+        final StockPriceWatcher stockPriceWatcher1 = new StockPriceWatcher(symbol1);
+        final StockPriceWatcher stockPriceWatcher2 = new StockPriceWatcher(symbol2);
 
         //series is the UI (View) element
-        final Series<String, Double> series = new Series<>(symbol, stockPriceWatcher.getData());
+        final Series<String, Double> series1 = new Series<>(symbol1, stockPriceWatcher1.getData());
+        final Series<String, Double> series2 = new Series<>(symbol2, stockPriceWatcher2.getData());
 
         //a chart supports more than one series of data, so the data for the chart is a list of series
         //but we only have one element in this list, our single series
-        chart.setData(observableArrayList(series));
+        chart.setData(observableArrayList(series1, series2));
 
 
         //A chart has multiple series
