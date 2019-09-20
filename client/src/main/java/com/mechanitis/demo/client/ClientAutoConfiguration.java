5e1086911fdf876f877e17d72bc5b309c450d5b5
Trisha Gee <trisha.gee@gmail.com>
2019-09-20 16:05:04 +0200

    Much simpler RSocket client config using the RSocketRequester.Builder

--- client/src/main/java/com/mechanitis/demo/client/ClientAutoConfiguration.java
+++ client/src/main/java/com/mechanitis/demo/client/ClientAutoConfiguration.java
@@ -1,19 +1,10 @@
 package com.mechanitis.demo.client;
 
-import io.rsocket.RSocket;
-import io.rsocket.RSocketFactory;
-import io.rsocket.frame.decoder.PayloadDecoder;
-import io.rsocket.transport.netty.client.TcpClientTransport;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.rsocket.MetadataExtractor;
 import org.springframework.messaging.rsocket.RSocketRequester;
-import org.springframework.messaging.rsocket.RSocketStrategies;
-import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.reactive.function.client.WebClient;
-
-import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON;
 
 @Configuration
 public class ClientAutoConfiguration {
@@ -29,30 +20,10 @@ public class ClientAutoConfiguration {
     }
 
     @Bean
-    RSocket rSocket() {
-        return RSocketFactory.connect()
-                             .mimeType(MetadataExtractor.ROUTING.toString(), MimeTypeUtils.APPLICATION_JSON_VALUE)
-                             .frameDecoder(PayloadDecoder.ZERO_COPY)
-                             .transport(TcpClientTransport.create(7000))
-                             .start()
-                             .block();
+    RSocketRequester rSocketRequester() {
+        return RSocketRequester.builder()
+                               .connectTcp("localhost", 7000)
+                               .block();
     }
-
-    @Bean
-    RSocketRequester rSocketRequester(RSocketStrategies rSocketStrategies) {
-        return RSocketRequester.wrap(rSocket(), APPLICATION_JSON, MetadataExtractor.ROUTING, rSocketStrategies);
-    }
-
-//    @Bean
-//    RSocketRequester requester(RSocketStrategies strategies) throws URISyntaxException {
-//        return RSocketRequester.builder().rsocketStrategies(strategies)
-//                               .rsocketFactory(factory -> {
-//                                   factory.dataMimeType(MimeTypeUtils.ALL_VALUE)
-//                                          .frameDecoder(PayloadDecoder.ZERO_COPY);
-//                               })
-//                               .connect(TcpClientTransport.create(new InetSocketAddress(
-//                                       clientConfigProp.getHost(), clientConfigProp.getPort())))
-//                               .retry().block();
-//    }
 
 }
--- client/src/test/java/com/mechanitis/demo/client/RSocketClientIntegrationTest.java
+++ client/src/test/java/com/mechanitis/demo/client/RSocketClientIntegrationTest.java
@@ -3,13 +3,12 @@ package com.mechanitis.demo.client;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.rsocket.RSocketStrategiesAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.messaging.rsocket.RSocketRequester;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes= {ClientAutoConfiguration.class, RSocketStrategiesAutoConfiguration.class})
+@SpringBootTest(classes = ClientAutoConfiguration.class)
 public class RSocketClientIntegrationTest {
 
     @Autowired
@@ -17,31 +16,9 @@ public class RSocketClientIntegrationTest {
 
     @Test
     public void shouldConnectToAnRSocketBackEnd() {
-
         new RSocketClient(rSocketRequester).pricesFor("RSOCKET_TEST")
                                            .take(10)
                                            .log()
                                            .blockLast();
     }
-
-//    @Configuration
-//    public static class ClientConfiguration {
-//
-//        @Bean
-//        @Lazy
-//        public RSocket rSocket() {
-//            return RSocketFactory.connect()
-//                                 .mimeType(MimeTypeUtils.APPLICATION_JSON_VALUE, MimeTypeUtils.APPLICATION_JSON_VALUE)
-//                                 .frameDecoder(PayloadDecoder.ZERO_COPY)
-//                                 .transport(TcpClientTransport.create(7000))
-//                                 .start()
-//                                 .block();
-//        }
-//
-//        @Bean
-//        @Lazy
-//        RSocketRequester rSocketRequester(RSocketStrategies rSocketStrategies) {
-//            return RSocketRequester.wrap(rSocket(), APPLICATION_JSON, APPLICATION_JSON, rSocketStrategies);
-//        }
-//    }
 }
