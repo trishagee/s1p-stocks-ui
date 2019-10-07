8d714912d34380a735c04f07cd3b9f83accd03c9
Trisha Gee <trisha.gee@gmail.com>
2019-10-07 14:49:53 -0500

    Removing StockPrices an putting the logic back into StockController. I like the StockPrices separation but it's too much complexity for the demo

--- ui/src/main/java/com/mechanitis/demo/ui/StockController.java
+++ ui/src/main/java/com/mechanitis/demo/ui/StockController.java
@@ -1,18 +1,26 @@
 package com.mechanitis.demo.ui;
 
 import com.mechanitis.demo.client.StockClient;
+import com.mechanitis.demo.client.StockPrice;
+import javafx.application.Platform;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart;
+import javafx.scene.chart.XYChart.Series;
 import org.springframework.stereotype.Component;
+
+import java.util.function.Consumer;
 
 import static javafx.collections.FXCollections.observableArrayList;
 
 @Component
-public class StockController {
+public class StockController implements Consumer<StockPrice> {
 
     @FXML
     private LineChart<String, Double> chart;
     private final StockClient stockClient;
+    private final ObservableList<XYChart.Data<String, Double>> seriesData = observableArrayList();
 
     StockController(final StockClient stockClient) {
         this.stockClient = stockClient;
@@ -21,12 +29,18 @@ public class StockController {
     //this is the controller, and what it does is wires stuff together
     @FXML
     public void initialize() {
-        final StockPrices stockPrices1 = new StockPrices("MDB", stockClient);
-        final StockPrices stockPrices2 = new StockPrices("GOOG", stockClient);
+        String symbol = "SYMBOL";
+        ObservableList<Series<String, Double>> data = observableArrayList(new Series<>(symbol, seriesData));
+        chart.setData(data);
 
-        //a chart supports more than one series of data, so the data for the chart is a list of series
-        chart.setData(observableArrayList(stockPrices1.getSeries(),
-                                          stockPrices2.getSeries()));
+        stockClient.pricesFor(symbol).subscribe(this);
     }
 
+    @Override
+    public void accept(StockPrice stockPrice) {
+        Platform.runLater(() ->
+                                  seriesData.add(new XYChart.Data<>(String.valueOf(stockPrice.getTime().getSecond()),
+                                                                    stockPrice.getPrice()))
+        );
+    }
 }
