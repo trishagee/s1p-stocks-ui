e95988a65f5846bca28f6e9cc8e2c77d0ac65b5f
Trisha Gee <trisha.gee@gmail.com>
2019-10-07 14:42:16 -0500

    Better integration test for WebClientStockClient

--- client/src/test/java/com/mechanitis/demo/client/WebClientStockClientIntegrationTest.java
+++ client/src/test/java/com/mechanitis/demo/client/WebClientStockClientIntegrationTest.java
@@ -1,17 +1,32 @@
 package com.mechanitis.demo.client;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 
 class WebClientStockClientIntegrationTest {
 
+    private WebClient webClient;
+
     @Test
     void shouldConnectToServiceAndReturnValues() {
-        Flux<StockPrice> prices = new WebClientStockClient(WebClient.builder().build()).pricesFor("TEST");
+        webClient = WebClient.builder().build();
+        String symbol = "TEST";
+        Flux<StockPrice> prices = new WebClientStockClient(webClient).pricesFor(symbol)
+                                                                     .take(5)
+                                                                     .log();
 
-        prices.take(10)
-              .log()
-              .blockLast();
+        Assertions.assertEquals(5, prices.count().block());
+
+        StockPrice stockPrice = prices.blockLast();
+        Assertions.assertNotNull(stockPrice);
+
+        Double price = stockPrice.getPrice();
+        Assertions.assertNotNull(price);
+        Assertions.assertTrue(price > 0 && price <= 100.0);
+
+        Assertions.assertEquals(symbol, stockPrice.getSymbol());
+        Assertions.assertNotNull(stockPrice.getTime());
     }
 }
