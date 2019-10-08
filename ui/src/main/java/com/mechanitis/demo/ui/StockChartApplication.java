a749122346a8fbff37fd53443faf7bf483df56f5
Trisha Gee <trisha.gee@gmail.com>
2019-10-08 11:08:36 -0500

    Updated to RC1, and did a tiny bit of tidying

--- ui/src/main/java/com/mechanitis/demo/ui/StockChartApplication.java
+++ ui/src/main/java/com/mechanitis/demo/ui/StockChartApplication.java
@@ -6,36 +6,33 @@ import javafx.application.Platform;
 import javafx.stage.Stage;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class StockChartApplication extends Application {
 
-
-    private ApplicationContext context;
+    private ConfigurableApplicationContext context;
 
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage stage) {
         this.context.publishEvent(new StageReadyEvent(stage));
     }
 
     @Override
-    public void init() throws Exception {
+    public void init() {
         this.context = new SpringApplicationBuilder()
                 .sources(StockChartApplication.class)
                 .run();
     }
 
-    //do we need this? I did run out of memory earlier....
     @Override
-    public void stop() throws Exception {
-//        this.context.close();
+    public void stop() {
+        this.context.close();
         Platform.exit();
     }
 
     static class StageReadyEvent extends ApplicationEvent {
-
         public StageReadyEvent(Stage stage) {
             super(stage);
         }
